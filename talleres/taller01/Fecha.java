8dedee5e21d0534a05f042afd72545e2c270ad38
juan9572 <68928340+juan9572@users.noreply.github.com>
2020-07-29 08:24:47 -0500

    Add files via upload

--- talleres/taller01/Fecha.java
+++ talleres/taller01/Fecha.java
@@ -1,87 +1,88 @@
 /**
  * La clase Fecha tiene la intención de representar el tipo abstracto fecha.
- * Aunque ya existen librerías encargadas de esta funcion:
- * @see <a href="http://www.baeldung.com/java-8-date-time-intro"> Ver documentacion tipo Time </a>
- * En este ejercicio podemos tener una compresión más profunda de su funcionamiento interno.
- * 
- * @author Mauricio Toro, Andres Paez
+ *
+ * @author Julian Gomez, Juan Pablo Rincon.
  * @version 1
  */
-
 public class Fecha {
-    
 
-    /*
-    varibales con atributo final indican que una variable es de tipo 
-    constante, es decir, no admitirá cambios después de su declaración y asignación de valor.
-    final determina que un atributo no puede ser sobreescrito o redefinido.
-    Se le asigna esta característica para evitar que se sobrescriban valores.
-    tener en cuenta tipado de las 3 variables!.
-    */
-
-    private final int dia;
-    private final int mes;
-    private final int anyo;
-
+    private int dia;
+    private int mes;
+    private int año;
 
     /**
-     * Se inicializan las variables globales en el constructor de manera que no posean valores nulos o 0s.
+     *Se inicializan los atributos de clase
      */
-    public Fecha() {
-
+    public Fecha(int dia, int mes, int año) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
     }
 
     /**
      * Método para obtener la variable global dia.
      *
-     * @return el dia
+     * @return el día en que se encuentra la variable.
      */
-    public int dia() {
-
+    public int dia(){
+        return this.dia;
     }
 
     /**
      * Método para obtener la variable global mes.
      *
-     * @return el mes
+     * @return el mes en que se encuentra la variable.
      */
-    public int mes() {
-
+    public int mes(){
+        return this.mes;
     }
 
     /**
-     * Método para obtener la variable global anio.
+     * Método para obtener la variable global año.
      *
-     * @return el año
+     * @return el año en que se encuentra la variable.
      */
-    public int anio() {
-
+    public int año(){
+        return this.año;
     }
 
     /**
-    * @param otra representa la fecha con la cual se va a comparar.
-    *
-    * El método comprar se encarga de devolvernos respuesta a tres posibilidades.
-    * 1: si la fecha es menor que la otra retorna -1.
-    * 2: si la fecha es igual que la otra retorna 0.
-    * 3: si la fecha es mayor que la otra retorna 1.
-    *
-    * @return -1 sí es menor; 0 sí es igual; 1 sí es mayor.
-    *
-    */
-
-    public int comparar(Fecha otra) {
-
+     *  Método para comparar fechas, retorna los siguientes digitos respectivamente:
+     *
+     *    -1 si esta fecha es anterior a la otra
+     *    0 si son iguales
+     *    1 si esta fecha es posterior a la otra
+     *
+     */
+    public int compararFecha(Fecha otra){
+        //Años
+        if(this.año < otra.año){
+            return -1;
+        }
+        if(this.año > otra.año){
+            return 1;
+        }
+        //Mes
+        if(this.mes < otra.mes){
+            return -1;
+        }if(this.mes < otra.mes){
+            return -1;
+        }
+        //Día
+        if(this.dia < otra.dia){
+            return -1;
+        }
+        if(this.dia < otra.dia){
+            return -1;
+        }
+        return 0;
     }
 
-
-     /**
-    * toString se encargará de convertir el tipo abstracto fecha en un tipo cadena
-    * para su posterior visualización
-    *
-    * @return una cadena que contiene la fecha
-    */
-    public String toString() {
-        
+    /**
+     *  Método toString para poder visualizar las fechas
+     * @return la fecha del objeto.
+     */
+    public String toString(){
+        return "Dia: " + dia + "\n" + "Mes: " + mes + "\n" + "Año: " + año;
     }
 }
