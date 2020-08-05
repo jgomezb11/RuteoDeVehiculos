791cda15c19db03c8819a16eef1115b4758bae96
jgomezb11 <60365681+jgomezb11@users.noreply.github.com>
2020-08-05 10:16:50 -0500

    Add files via upload
    Puntos 1, 2 y 3 hechos.
    Punto 4 a medias, falta valor de retorno.

---
+++ talleres/taller02/Taller2.java
@@ -0,0 +1,110 @@
+/**
+ *La clase taller dos tiene como objetivo dar solución
+ *a 3 problemas recursivos
+ *
+ *@autor Juan Pablo Rincon Usma, Julian Gomez Benitez
+ *@version 1
+ */
+
+public class Taller2 {
+
+    /**
+     * @param p entrada 1 entero positivo, mayor que q
+     * @param q entrada 2 entero positivo, menor que p
+     *
+     * El método gcd tiene como objetivo ecnontrar el
+     * máximo común divisor de dos números, por medio del
+     * algoritmo de euclides
+     * @see <a href="https://www.youtube.com/watch?v=Q9HjeFD62Uk"> Explicación </a>
+     * @see <a href="https://visualgo.net/en/recursion"> Funcionamiento </a>
+     *
+     * @return el máximo común divisor
+     */
+    public static int gcd(int p,int q){ // p > q
+        if (q == 0)
+            return p;
+        else
+            return gcd(q, p % q);
+    }
+
+    /**
+     * @param nums entrada 2 arreglo de enteros positivos, sobre el cual vamos a interar 
+     * @param target entrada 3 entero positivo, determina el valor de referencia 
+     * El método SumaGrupo tiene como objetivo darnos a conocer si hay 
+     * algun subconjunto el cual su suma = target.
+     * 
+     *
+     * @return verdadero si hay un subconjunto el cual su suma = target
+     */
+    public static boolean SumaGrupo(int[] nums, int target) {
+        return SumaGrupo(0, nums, target);
+    }   
+
+    /**
+     * @param start entrada 1 entero positivo, determina un índice dentro del proceso
+     * @param nums entrada 2 arreglo de enteros positivos, sobre el cual vamos a interar 
+     * @param target entrada 3 entero positivo, determina el valor de referencia 
+     * El método SumaGrupo tiene como objetivo darnos a conocer si hay 
+     * algun subconjunto el cual su suma = target.
+     * 
+     * Este método SumaGrupo es "private" de modo que solo se puede llamar desde el interior de la clase pues
+     * el método que lo representa es el SumaGrupo público.
+     * Para más detalles sobre modificadores de acceso:
+     * @see <a href="http://ayudasprogramacionweb.blogspot.com/2013/02/modificadores-acceso-public-protected-private-java.html"> modificadores </a>
+     *
+     *
+     * @return verdadero si hay un subconjunto el cual su suma = target
+     */
+    private static boolean SumaGrupo(int start, int[] nums, int target) {
+        if(target == 0) return true;
+        if(start == nums.length) return false;
+        return SumaGrupo(start + 1, nums, target - nums[start]) ||
+        SumaGrupo(start+1, nums, target);
+    }
+
+    /**
+     * @param s se trata de una cadena de caracteres sobre la cual hallaremos las posibles combinaciones.
+     *
+     * El método combinations se define para que solo se tenga que pasar el parametro s y no la cadena 
+     * vacía necesaria para el metodo reursivo combinationsAux. Este metodo no se modifica.
+     * 
+     */
+    public static void combinations(String s) { 
+        combinationsAux("", s); 
+    }
+
+    /**
+     * @param prefix, se utiliza como una variable auxiliar para guardar datos sobre el proceso.
+     * @param s se trata de una cadena de caracteres sobre la cual hallaremos las posibles combinaciones.
+     *
+     *
+     * El método combinationsAux se encarga de encontrar las posibles combinaciones en la cadena s
+     * notese que el método es "private" de modo que solo se puede llamar desde el interior de la clase pues
+     * el método que lo representa es combinations.
+     * Para más detalles sobre modificadores de acceso:
+     * @see <a href="http://ayudasprogramacionweb.blogspot.com/2013/02/modificadores-acceso-public-protected-private-java.html"> modificadores </a>
+     *
+     */
+
+    private static void combinationsAux(String prefix, String s) {
+        if(s.length() == 0) {
+            System.out.println(prefix);
+        }else{
+            combinationsAux( prefix + s.charAt(0), s.substring(1));
+            combinationsAux( prefix, s.substring(1));
+        }
+    }
+
+    public static int lcs(String x, String y) {
+        if((x.length() != 0) && (y.length() != 0))
+            if(x.charAt(x.length() - 1) == y.charAt(y.length() -1)){
+                lcs(x.substring(0,x.length() - 1), y.substring(0, y.length() - 1));
+
+            }else{
+                lcs(x, y.substring(0, y.length() - 1));
+                lcs(x.substring(0,x.length() - 1), y);
+            }
+        return ??;
+    }
+
+}
