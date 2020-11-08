f332c58aa227085bb59d769e0df58aacf436117d
juan9572 <68928340+juan9572@users.noreply.github.com>
2020-11-07 23:51:05 -0500

    Add files via upload

---
+++ laboratorios/lab05/ejercicioEnLinea/Algorithm.java
@@ -0,0 +1,14 @@
+package Lab05.EjercicioEnLinea;
+
+import Taller12.DiagraphAM;
+
+public class Algorithm {
+
+    public static boolean DFSColorFC(DigraphAM am,Node node) {
+        return DFSColorFCAux(node, 1);
+    }
+
+    private static boolean DFSColorFCAux(Node first, int color) {
+
+    }
+}
---
+++ laboratorios/lab05/ejercicioEnLinea/Main.java
@@ -0,0 +1,15 @@
+package Lab05.EjercicioEnLinea;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scan n = new Scan();
+        try {
+            n.readBicolor();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
